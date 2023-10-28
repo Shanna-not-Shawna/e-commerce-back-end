da189aacd9a47f4f894b464aa0a2072820d298b1
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-10-27 23:34:50 -0500

    fixed bugs

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -9,7 +9,7 @@ ProductTag.init(
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey; true,
+      primaryKey: true,
       autoIncrement: true,
     },
     product_id: {
